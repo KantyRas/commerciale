495888589ac6e632f996777aab12d557d9924da6
KantyRas <rasolofomananakanty@gmail.com>
2023-11-26 19:18:54 +0300

    modif

--- application/controllers/Departement.php
+++ application/controllers/Departement.php
@@ -2,6 +2,11 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 class Departement extends CI_Controller
 {
+    public function __construct()
+    {
+
+    }
+
     public function departements(){
         $this->load->view('accueil');
         $this->load->view('departement');
